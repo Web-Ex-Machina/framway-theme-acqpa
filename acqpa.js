d89786156beab4c6578198336e18cd950a50ffa0
LupusVII <jln.thirion@gmail.com>
2022-05-13 18:11:11 +0200

    Add postData function and reload callback

--- acqpa.js
+++ acqpa.js
@@ -1,3 +1,31 @@
 $(function(){
 
 });
+
+/** UTILITIES **/
+utils.postData = async function postData(data, url = "", method = "POST") {
+	var request = new FormData();
+	request.append('TL_AJAX', 1);
+
+	for(var i in data) {
+		request.append(i, data[i]);
+	}
+
+	var strUrl = url ? url : window.location.href;
+	var strMethod = method ? method: "POST";
+
+	const response = await fetch(strUrl, {
+		method: strMethod,
+		mode: 'same-origin',
+		cache: 'no-cache',
+		body: request
+	});
+
+	return response.json();
+};
+
+/** CALLBACKS **/
+utils.callbacks = {};
+utils.callbacks.reload = function reloadPage(wait = 600) {
+	setTimeout(() => { window.location.reload(false) }, wait);
+}
