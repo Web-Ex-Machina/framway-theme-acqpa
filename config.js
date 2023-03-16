8cd480e8a93ed9236163c42d1696504dcd2cd18d
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-03-16 18:36:46 +0100

    add some config vars and styles for lists

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'blue' : '#0073BB',
+	},
+	'primary': 'colors(blue)',
+	'body': {
+		'font-color': 'colors(greystronger)',
+		'font-size'		: '16px',
+		'font-size-xxs'	: '14px',
+	},
+};
