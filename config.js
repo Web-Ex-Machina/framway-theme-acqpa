832c2ecfd1c37b25c65e6077f32e24421eb518aa
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-03-29 14:52:26 +0200

    set d-grid default gap to 1em remove the w-full effect on table-list's modules style the tab-like menu in edit modules increase default container width

--- config.js
+++ config.js
@@ -8,4 +8,12 @@ module.exports = {
 		'font-size'		: '16px',
 		'font-size-xxs'	: '14px',
 	},
+	'container':{
+		'xl'  : '1320px', // 1320px
+		'lg'  : '1140px', // 1140px
+		// 'md'  : '960px',
+		// 'sm'  : '720px',
+		// 'xs'  : '540px',
+		// 'xxs' : '100%',
+	},
 };
