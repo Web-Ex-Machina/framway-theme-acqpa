1bd5ca05a4b206710a6238f8affb23ce2c16f41f
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-04-07 18:35:22 +0200

    soften info and warning colors

--- config.js
+++ config.js
@@ -8,6 +8,11 @@ module.exports = {
 		'font-size'		: '16px',
 		'font-size-xxs'	: '14px',
 	},
+	// 'success': 'colors(green)',
+	'info': 'rgba(colors(blue),0.75)',
+	'warning': 'rgba(colors(yellow),0.65)',
+	// 'warning': 'colors(yellow)',
+	// 'error': 'colors(red)',
 	'container':{
 		'xl'  : '1320px', // 1320px
 		'lg'  : '1140px', // 1140px
