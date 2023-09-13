9dec3eb7bdd964bd393a7dfc0fae1420a28021a3
ChipsVII <benjamin@webexmachina.fr>
2023-09-13 17:33:40 +0200

    Fix : new way to open file in new tab

--- acqpa.js
+++ acqpa.js
@@ -1344,13 +1344,16 @@ acqpa.utils.callbacks.redirect = function redirect(url, wait = 600) {
 	setTimeout(() => { window.location.replace(url) }, wait);
 }
 acqpa.utils.callbacks.openInNewTab = function openInNewTab(url) {
-	const a = document.createElement('a');
-	a.style.display = 'none';
-	a.href = url;
-	a.target = "_blank";
-	document.body.appendChild(a);
-	a.click();
-	a.remove();
+	// const a = document.createElement('a');
+	// a.style.display = 'none';
+	// a.href = url;
+	// a.target = "_blank";
+	// document.body.appendChild(a);
+	// a.click();
+	// a.remove();
+	
+	var win = window.open();
+  win.document.write('<iframe src="' + url  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
 }
 acqpa.utils.callbacks.openModal = function openModal(args) {
 	let obj = {onClose: ()=>{modal.destroy();}};
